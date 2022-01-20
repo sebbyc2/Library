8693d9f7b9814cc988fa397807483f1e63e5912a
luizzitol <luizzitol@gmail.com>
2022-01-20 14:44:28 +0000

    fixed camera describeit method

--- src/main/java/com/qa/Library/Library.java
+++ src/main/java/com/qa/Library/Library.java
@@ -3,17 +3,27 @@ package com.qa.Library;
 import java.util.List;
 
 import com.qa.items.LibraryItem;
+import com.qa.person.Person;
 
 public class Library {
 
-	private List<LibraryItem> allItems;
+	private List<LibraryItem> items;
+	private List<Person> members;
 
 	public void addItem(LibraryItem item) {
-		allItems.add(item);
+		items.add(item);
 	}
 
 	public void removeItem(LibraryItem item) {
-		allItems.remove(item);
+		items.remove(item);
+	}
+
+	public void addMember(Person person) {
+		members.add(person);
+	}
+
+	public void removeMember(Person person) {
+		members.remove(person);
 	}
 
 }
--- src/main/java/com/qa/items/Camera.java
+++ src/main/java/com/qa/items/Camera.java
@@ -1,51 +1,56 @@
 package com.qa.items;
 
 public class Camera extends LibraryItem {
-	
+
 	public Camera(String name, int fee, String brand, int fieldOfView, String lenses, double shutterSpeed) {
 		super(name, fee);
 		this.setBrand(name);
 		this.setFieldOfView(fieldOfView);
 		this.setLenses(name);
 		this.setShutterSpeed(shutterSpeed);
-	
+
 	}
+
 	private int fieldOfView;
 	private double shutterSpeed;
 	private String lenses;
 	private String brand;
-	
+
 	public int getFieldOfView() {
 		return fieldOfView;
 	}
+
 	public void setFieldOfView(int fieldOfView) {
 		this.fieldOfView = fieldOfView;
 	}
+
 	public double getShutterSpeed() {
 		return shutterSpeed;
 	}
+
 	public void setShutterSpeed(double shutterSpeed) {
 		this.shutterSpeed = shutterSpeed;
 	}
+
 	public String getLenses() {
 		return lenses;
 	}
+
 	public void setLenses(String lenses) {
 		this.lenses = lenses;
 	}
+
 	public String getBrand() {
 		return brand;
 	}
+
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
+
 	@Override
 	public void throwIt() {
-		
-	}
-	@Override
-	public void describeIt() {
-		
+
 	}
 
 }
